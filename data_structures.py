e9106409548df47ca0800579f4ec64d02e18d7cb
ondrej-karlicek <ondrej.karlicek.ih@gmail.com>
2021-08-29 14:45:49 +0200

    finished vaccination_centers_scrapper

--- data_structures.py
+++ data_structures.py
@@ -3,6 +3,14 @@ class VaccCenter():
         self.name = name
         self.region = region
         self.link = link
+        self.info = {}
+        self.open_hours = {}
+
+    def add_info(self, info):
+        self.info = info
+
+    def add_open_hours(self, open_hours):
+        self.open_hours = open_hours
 
     def __repr__(self):
         return f"VaccCenter({self.name})"
--- vaccination_centers_scrapper.py
+++ vaccination_centers_scrapper.py
@@ -1,4 +1,5 @@
 import mechanicalsoup
+import requests
 from bs4 import BeautifulSoup
 from time import sleep
 from config import URL_VACCENTERS, REGIONS
@@ -23,6 +24,15 @@ class VaccCentersScraper():
         browser.close()
         return None
 
+    def get_information_about_centers(self):
+        for center in self.vacc_centers:
+            soup = BeautifulSoup(requests.get(center.link).text.strip())
+            center.add_info(self._extract_info_from_soup(soup))
+            center.add_open_hours(self._extract_open_hours_from_soup(soup))
+            sleep(0.5)
+        return None
+
+
     def _extract_links_from_soup(self, soup, region):
         centers_div = soup.select('div[class=center__container]')
         for center in centers_div:
@@ -33,3 +43,37 @@ class VaccCentersScraper():
                 self.vacc_centers.append(VaccCenter(name, region, link))
         return None
 
+
+    def _extract_info_from_soup(self, soup):
+        table_info = soup.select('div[class=info] > table > tbody > tr')
+        info = {i.select_one('td:nth-child(1)').text: i.select_one('td:nth-child(2)').text.replace("\r\n", "")
+                for i in table_info[0:4]}
+        info['Poznámka'] = table_info[4].select_one('td:nth-child(2)').text.replace("\t", " ").replace("\r\n", "")
+
+        info['Vakcíny'] = [t.text for t in table_info[5].select_one('td:nth-child(2)').select('div[class=vaccineName]')]
+
+        info['Dodatečné informace'] = [t.text.replace("👨\u200d🦽 ", "") for t in
+                                       table_info[6].select_one('td:nth-child(2)').select('span')]
+
+        info['Denní kapacita očkování'] = table_info[7].select_one('td:nth-child(2)').text.replace("\r\n", "")
+
+        info['Způsob změny termínu druhé dávky vakcíny'] = [t.text.replace('\n', '') for t in
+                                                            table_info[8].select_one('td:nth-child(2)' ).select('div')]
+        return info
+
+    def _extract_open_hours_from_soup(self, soup):
+        open_table = soup.select('div[class=detail__aside] > div[class=opening] > table > tbody > tr')
+        open_table = {i.select_one('td:nth-child(1)').text: i.select_one('td:nth-child(2)').text.strip().replace(" ", "")
+                      for i in open_table}
+        return open_table
+
+
+if __name__ == '__main__':
+    v = VaccCentersScraper()
+    v.get_links()
+    v.get_information_about_centers()
+    centers = v.vacc_centers
+    centers[0].name
+    centers[0].region
+    centers[0].info
+    centers[0].open_hours
