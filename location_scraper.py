36bb116951399660c3ced23a1292bccddceee81f
Vilém <v.krejcar@seznam.cz>
2021-08-31 21:26:30 +0200

    Updated scraper so it coverts degrees into decimals and also transforms whole dataset in a similar manner.

--- location_scraper.py
+++ location_scraper.py
@@ -28,6 +28,7 @@ def scrape_dataset() -> pd.DataFrame:
     i = 1
     for town in links:
         if BeautifulSoup(requests.get(town).text.strip()).h1.text == "Těšetice (okres Znojmo)":
+            #town Těšetice has different Wiki page structure, and thus we weren't able to add it into the scraper
             i += 1
             continue
         else:
@@ -42,3 +43,47 @@ def scrape_dataset() -> pd.DataFrame:
     data = pd.DataFrame(data)
     data.columns = ["Town name", "Latitude", "Longitude"]
     return data
+
+
+def prepare_to_convert(string: str) -> list:
+    degrees, minutes, seconds = True, True, True
+    if string == string.replace("°"," "):
+        degrees = False
+    if string == string.replace("′"," "):
+        minutes = False
+    if string == string.replace("″"," "):
+        seconds = False
+    string = string.replace("°"," ").replace("′"," ").replace("″", " ").split(" ")
+    del(string[-1])
+    if seconds is False:
+        string.append(0)
+    if minutes is False:
+        string.insert(0, 1)
+    if degrees is False:
+        string.insert(0, 0)
+    for i in range(len(string)):
+         string[i] = float(string[i])
+    return string
+
+
+def degrees_to_decimal(degrees: int = 0, minutes: int = 0, seconds: int = 0) -> float:
+    result = 0
+    result += degrees
+    result += minutes/60
+    result += seconds/3600
+    return result
+
+
+def reorganize_dataset(dataset: pd.DataFrame) -> pd.DataFrame():
+    for i in range(dataset.shape[0]):
+        new_long = prepare_to_convert(dataset["Longitude"][i])
+        new_lat = prepare_to_convert(dataset["Latitude"][i])
+        dataset["Longitude"][i] = degrees_to_decimal(new_long[0], new_long[1], new_long[2])
+        dataset["Latitude"][i] = degrees_to_decimal(new_lat[0], new_lat[1], new_lat[2])
+    return dataset
+
+
+def add_town(dataframe: pd.DataFrame, name: str, latitude: float, longitude: float) -> pd.DataFrame:
+    new_datapoint = {"Town name": name, "Latitude": latitude, "Longitude": longitude}
+    dataframe = dataframe.append(new_datapoint, ignore_index=True)
+    return dataframe
